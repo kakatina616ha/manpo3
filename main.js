a039d1a2bf4f2f3c740208987e443b9b061b4322
kakatina616ha <68804047+kakatina616ha@users.noreply.github.com>
2021-09-05 15:07:34 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const nav = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active')
+}
+
+
